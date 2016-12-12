8862756faf1bfd3e149a2b7f0379468f928d4f46
Vladutu <vladutu_georgian_4d@yahoo.com>
2016-12-12 14:34:12 +0200

    - content solution returned as object

---
+++ domain/src/main/java/ro/ucv/ace/utility/impl/Content.java
@@ -0,0 +1,21 @@
+package ro.ucv.ace.utility.impl;
+
+/**
+ * Created by Geo on 12.12.2016.
+ */
+public class Content {
+
+    private String content;
+
+    public Content(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
